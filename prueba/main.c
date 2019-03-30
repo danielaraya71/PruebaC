c596a58b346018cfd315ef2ac27d8d7487cc513b
danie <danie@LAPTOP-VB4EU9DH>
2019-03-30 14:54:00 -0600

    prueba c

---
+++ prueba/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: danie
+ *
+ * Created on March 30, 2019, 2:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
